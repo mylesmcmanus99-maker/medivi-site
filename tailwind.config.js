e416e26990c5ae89b367f82f73caf6ee89a46d74
mylesmcmanus99-maker <myles.mcmanus99@gmail.com>
2026-04-06 14:49:58 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#1B263B',
+        teal: '#31B2BF',
+        slate: '#E0E1DD',
+        charcoal: '#415A77',
+      },
+    },
+  },
+  plugins: [],
+}
